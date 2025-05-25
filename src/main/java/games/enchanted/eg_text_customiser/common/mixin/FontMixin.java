2f6a98355be4daa41565f3b5a34557ea287fd7b1
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-05-25 23:56:36 +0100

    update font mixin

--- src/main/java/games/enchanted/eg_text_customiser/common/mixin/FontMixin.java
+++ src/main/java/games/enchanted/eg_text_customiser/common/mixin/FontMixin.java
@@ -2,21 +2,20 @@ package games.enchanted.eg_text_customiser.common.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.network.chat.Style;
-import net.minecraft.network.chat.TextColor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Coerce;
 
 @Mixin(targets = "net/minecraft/client/gui/Font$StringRenderOutput")
 public class FontMixin {
     @WrapOperation(
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Font$StringRenderOutput;getShadowColor(Lnet/minecraft/network/chat/Style;I)I"),
-        method = "Lnet/minecraft/client/gui/Font$StringRenderOutput;accept(ILnet/minecraft/network/chat/Style;I)Z"
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ARGB;scaleRGB(IF)I"),
+        method = "Lnet/minecraft/client/gui/Font$StringRenderOutput;getShadowColor(Lnet/minecraft/network/chat/Style;I)I"
     )
-    private int eg_text_customiser$replaceShadowColour(@Coerce Object instance, Style textStyle, int rawTextColour, Operation<Integer> original, @Local(ordinal = 0) TextColor textColour) {
-
-        return 0;
+    private int eg_text_customiser$replaceShadowColour(int originalColour, float originalScale, Operation<Integer> original, Style textStyle, int rawTextColour) {
+        if(textStyle.getColor() == null) {
+            return original.call(originalColour, originalScale);
+        }
+        return original.call(originalColour, 0.7f);
     }
 }
