3b5ba38329d95b803a2a61c4d1cdc7a39d99655c
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-10-20 21:21:44 +0100

    update style logging

--- src/main/java/games/enchanted/eg_text_customiser/common/fake_style/FakeStyle.java
+++ src/main/java/games/enchanted/eg_text_customiser/common/fake_style/FakeStyle.java
@@ -3,6 +3,8 @@ package games.enchanted.eg_text_customiser.common.fake_style;
 import games.enchanted.eg_text_customiser.common.pack.colour_override.ColourOverrideDefinition;
 import games.enchanted.eg_text_customiser.common.util.ColourUtil;
 import net.minecraft.network.chat.FontDescription;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.component.ResolvableProfile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -82,13 +84,21 @@ public record FakeStyle(
             (underlined == null ? "" : ", underlined=" + underlined) +
             (strikethrough == null ? "" : ", strikethrough=" + strikethrough) +
             (obfuscated == null ? "" : ", obfuscated=" + obfuscated) +
-            (font == null ? "" : ", font=\"" + formatFontDescription(font) + "\"") +
+            (font == null ? "" : ", font=" + formatFontDescription(font) + "") +
             (decorationType == null ? "" : ", decoration_type=\"" + decorationType + "\"") +
-            (codepoint == null ? "" : ", codepoint=\"" + codepoint + "\"") +
+            (codepoint == null ? "" : ", character=[codepoint=" + codepoint + ", char=" + (char)codepoint.intValue() + "]") +
         ']';
     }
 
     private static String formatFontDescription(FontDescription font) {
-        return "TODO"; //TODO
+        if(font instanceof FontDescription.Resource(ResourceLocation id)) {
+            return "\"" + id + "\"";
+        } else if(font instanceof FontDescription.AtlasSprite(ResourceLocation atlasId, ResourceLocation spriteId)) {
+            return "[type=atlas_sprite, atlas\"" + atlasId + "\", sprite\"" + spriteId + "\"]";
+        }
+        else if(font instanceof FontDescription.PlayerSprite(ResolvableProfile profile, boolean hat)) {
+            return "[type=profile, name\"" + profile.name().orElse("<none>") + "\", hat\"" + hat + "\"]";
+        }
+        return "unknown";
     }
 }
